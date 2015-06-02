9376cf73ab1a857f1452a8ee4729a8fcdafff3db
Loraine <loraine@lorainekv.com>
2015-06-02 15:32:08 -0700

    Got all pages working.

--- my-site.rb
+++ my-site.rb
@@ -8,4 +8,16 @@ class MySite < Sinatra::Base
     send_file 'public/index.html'
   end
 
+  get '/about' do
+    send_file 'public/about.html'
+  end
+
+  get '/blog' do
+    send_file 'public/blog.html'
+  end
+
+  get '/projects' do
+    send_file 'public/projects.html'
+  end
+
 end
