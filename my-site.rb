747ea16b6c4ef97e383bedc8af4c4d4118fb9cfa
Brandi Wilson <wilson.e.brandi@gmail.com>
2015-06-02 14:36:05 -0700

    The links work! Yay!

--- my-site.rb
+++ my-site.rb
@@ -5,6 +5,26 @@ class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
+    send_file './mah_html/home.html'
+  end
 
+  get "/home" do
+    send_file './mah_html/home.html'
+  end
+
+  get "/about" do
+    send_file './mah_html/about.html'
+  end
+
+  # get "/about.html" do
+  #   send_file './mah_html/about.html'
+  # end
+
+  get "/blog" do
+    send_file './mah_html/blog.html'
+  end
+
+  get "/projects" do
+    send_file './mah_html/projects.html'
   end
 end
