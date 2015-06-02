81f5a43f8e882909c2a5c1879c1c21cfa0296c08
Loren Wang <wang131.131@gmail.com>
2015-06-02 15:18:42 -0700

    first PR on my ugly website

--- my-site.rb
+++ my-site.rb
@@ -1,10 +1,28 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" # alter code in sinatra so no restart needed
 
 class MySite < Sinatra::Base
-  register Sinatra::Reloader
+  register Sinatra::Reloader # making line 2 active
 
   get "/" do
+    send_file 'static/home2.html'
+  end
 
+  get "/home" do
+    send_file 'static/home2.html'
+  end
+
+  get "/about" do
+    send_file 'static/about.html'
+  end
+
+  get "/projects" do
+    send_file 'static/projects.html'
+  end
+
+  get "/blog" do
+    send_file 'static/blog.html'
   end
 end
+
+# rackup to execute
