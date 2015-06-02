f394743efb59bdf9d93d05321d1f65cfd76add3f
Victoria <vikshab2010@hotmail.com>
2015-06-02 15:15:08 -0700

    1 wave is ready

--- my-site.rb
+++ my-site.rb
@@ -5,6 +5,19 @@ class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
-
+    send_file "public/home_page.html"
   end
+
+  get "/about" do
+    send_file "public/about.html"
+  end
+
+  get "/blog_post" do
+    send_file "public/blog_post.html"
+  end
+
+  get "/projects" do
+    send_file "public/projects.html"
+  end
+
 end
