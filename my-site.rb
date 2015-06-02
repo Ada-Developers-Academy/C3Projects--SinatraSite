9b99e5295567166178f66a415fdeb4ea2bdb39a3
Ashley Watkins <AWatkins31@gmail.com>
2015-06-02 15:19:24 -0700

    Creates initial (unpolished) website

--- my-site.rb
+++ my-site.rb
@@ -5,6 +5,26 @@ class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
+    send_file "./public/home.html"
+  end
 
+  get "/home" do
+    send_file "./public/home.html"
+  end
+
+  get "/posts" do
+    send_file "./public/posts.html"
+  end
+
+  get "/projects" do
+    send_file "./public/projects.html"
+  end
+
+  get "/about" do
+    send_file "./public/about.html"
+  end
+
+  get "/post1" do
+    send_file "./public/blog_post.html"
   end
 end
