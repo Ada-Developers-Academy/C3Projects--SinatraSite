1199df7ff76e32783c88bab544ca88ed74e6c4cb
EliaMG <EliaMG@users.noreply.github.com>
2015-06-02 16:35:23 -0700

    Revert "First wave PR"

--- my-site.rb
+++ my-site.rb
@@ -4,20 +4,7 @@ require "sinatra/reloader"
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
-  get '/' do
-    send_file './pages/home.html'
-  end
+  get "/" do
 
-  get '/blog' do
-    send_file './pages/blog.html'
   end
-
-  get '/about' do
-    send_file './pages/about.html'
-  end
-
-  get '/projects' do
-    send_file './pages/projects.html'
-  end
-
 end
