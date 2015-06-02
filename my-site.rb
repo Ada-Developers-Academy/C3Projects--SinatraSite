1a18af3d77e2752d12210e30d6b641d215ee1c77
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-06-02 15:25:22 -0700

    wave 1 complete. 4 sites up, blog, about, home and projects.

--- my-site.rb
+++ my-site.rb
@@ -1,10 +1,23 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" # makes it so we don't have to restart Sinatra
+  # when execute, do it through rackup in terminal
 
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
-  get "/" do
+  get "/home" do # what hte user is asking
+    send_file "public/home.html" # this is the response
+  end
 
+  get "/about" do
+    send_file "./public/about.html"
+  end
+
+  get "/blog" do
+    send_file "public/blog.html"
+  end
+
+  get "/projects" do
+    send_file "./public/projects.html"
   end
 end
