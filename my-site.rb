047c996bd6f0ea4cb232fda0b29238c9fab3b28c
marleigh <camarleigh@gmail.com>
2015-06-02 16:04:07 -0700

    i always forget to commit

--- my-site.rb
+++ my-site.rb
@@ -4,7 +4,19 @@ require "sinatra/reloader"
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
-  get "/" do
+  get "/home" do
+    send_file'public/Home.html'
+  end
 
+  get "/about_me" do
+    send_file'public/Me.html'
+  end
+
+  get "/current_projects" do
+    send_file'public/Projects.html'
+  end
+
+  get "/blog" do
+    send_file'public/Blog.html'
   end
 end
