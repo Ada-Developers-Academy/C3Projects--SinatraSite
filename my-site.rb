f0bcd927550e4011de7c729567e26b8b66adfa58
Lindsey Reno <lindseyannereno@gmail.com>
2015-06-02 16:01:19 -0700

    HTML of website

--- my-site.rb
+++ my-site.rb
@@ -4,7 +4,19 @@ require "sinatra/reloader"
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
-  get "/" do
+  get "/home" do
+    send_file "static_html/home_page.html"
+  end
 
+  get "/about" do
+    send_file "static_html/about_page.html"
+  end
+
+  get "/projects" do
+    send_file "static_html/projects_page.html"
+  end
+
+  get "/blog" do
+    send_file "static_html/blog_page.html"
   end
 end
