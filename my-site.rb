b4990a863ed7d50374e87592f22e46a17ada3554
Kari <kari.lonczak@gmail.com>
2015-06-02 14:41:45 -0700

    Added img files and html files to new Sinatra directory structure.

--- my-site.rb
+++ my-site.rb
@@ -5,6 +5,23 @@ class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
-
+  	send_file './public/index.html'
   end
+
+  get "/about_me" do
+  	send_file './static_html/about_me.html'
+  end
+
+  get "/pictures" do
+  	send_file './static_html/pictures.html'
+  end
+
+  get "/places" do
+  	send_file './static_html/places.html'
+  end
+
+  get "/projects" do
+  	send_file './static_html/projects.html'
+  end
+
 end
