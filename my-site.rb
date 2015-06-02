25d112e297f07f71d1509e5a4cb928b4bb2e8b37
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-02 14:43:28 -0700

    sinatra project initial commit

--- my-site.rb
+++ my-site.rb
@@ -2,9 +2,26 @@ require "sinatra"
 require "sinatra/reloader"
 
 class MySite < Sinatra::Base
-  register Sinatra::Reloader
+ 	register Sinatra::Reloader
 
-  get "/" do
+ 	# get "/" do
+	 #  	"<h1>Yay, no restarting. And I can modify it.</h1>"
+ 	# end
 
-  end
+	get '/' do
+		send_file './stuff/home.html'
+	end
+
+	get '/about' do
+		send_file './stuff/about.html'
+	end
+
+	get '/blog' do
+		send_file './stuff/blog.html'
+	end
+
+	get '/projects' do
+		send_file './stuff/projects.html'
+	end
+
 end
