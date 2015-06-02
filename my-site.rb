33252e00a5438af5f0819169154393d96d6da3d2
Corinne Pingul <corinnepingul@gmail.com>
2015-06-02 14:21:02 -0700

    Added four html files. Referenced them via sinatra routes.

--- my-site.rb
+++ my-site.rb
@@ -1,10 +1,25 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" # lets us update this code without having to restart Sinatra every time
+
+# running in terminal => rackup
 
 class MySite < Sinatra::Base
-  register Sinatra::Reloader
+  register Sinatra::Reloader # makes line 2 active
 
   get "/" do
-
+    send_file "static_html/index.html"
   end
+
+  get "/about" do
+    send_file "static_html/about.html"
+  end
+
+  get "/blog_posts" do
+    send_file "static_html/blog_posts.html"
+  end
+
+  get "/projects" do
+    send_file "static_html/projects.html"
+  end
+
 end
