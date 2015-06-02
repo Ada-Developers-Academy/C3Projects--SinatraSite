c321a77b33f1ef03d610600c9538c08a3db5f1d7
Jeri <drvonnjerryxlii@gmail.com>
2015-06-02 14:17:02 -0700

    pretty sure I've finished wave 1

--- my-site.rb
+++ my-site.rb
@@ -5,6 +5,34 @@ class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
+    send_file 'views/index.html'
+  end
 
+  get "/index.html" do
+    redirect '/'
+  end
+
+  get "/about" do
+    send_file 'views/about.html'
+  end
+
+  get "/blag" do
+    send_file 'views/blag.html'
+  end
+
+  get "/projects" do
+    send_file 'views/projects.html'
+  end
+
+  get "/projects/solar-system" do
+    send_file 'views/projects/solar-system.html'
+  end
+
+  get "/projects/set-clone" do
+    send_file 'views/projects/set-clone.html'
+  end
+
+  get "/projects/euler-problems" do
+    send_file 'views/projects/euler-problems.html'
   end
 end
