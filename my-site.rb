058ccf9a2b2fb7774c7dad16c9d5f83e192d1ca5
Lila Garcia <lilagrc@gmail.com>
2015-06-02 15:18:44 -0700

    Completed first wave

--- my-site.rb
+++ my-site.rb
@@ -4,7 +4,21 @@ require "sinatra/reloader"
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
-  get "/" do
 
+  get '/home' do
+    send_file 'public/home_page.html'
   end
+
+  get '/about' do
+    send_file 'public/about.html'
+  end
+
+  get '/projects' do
+    send_file 'public/projects.html'
+  end
+
+  get '/blog' do
+    send_file 'public/blog.html'
+  end
+
 end
