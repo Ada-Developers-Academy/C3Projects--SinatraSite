548b574e73ff4ec1926f18b357d5b6386ce2e661
Kari <kari.lonczak@gmail.com>
2015-06-04 14:38:11 -0700

    created new view files

--- my-site.rb
+++ my-site.rb
@@ -8,20 +8,24 @@ class MySite < Sinatra::Base
   	send_file './public/index.html'
   end
 
+  # get "/about_me" do
+  # 	send_file './static_html/about_me.html'
+  # end
+
+  # get "/pictures" do
+  # 	send_file './static_html/pictures.html'
+  # end
+
+  # get "/places" do
+  # 	send_file './static_html/places.html'
+  # end
+
+  # get "/projects" do
+  # 	send_file './static_html/projects.html'
+  # end
+
   get "/about_me" do
-  	send_file './static_html/about_me.html'
-  end
-
-  get "/pictures" do
-  	send_file './static_html/pictures.html'
-  end
-
-  get "/places" do
-  	send_file './static_html/places.html'
-  end
-
-  get "/projects" do
-  	send_file './static_html/projects.html'
+    erb :about_me
   end
 
 end
