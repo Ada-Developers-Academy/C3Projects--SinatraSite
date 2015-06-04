1c7a3bdf02df253be68711dc94ec8e19b56691b5
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-04 14:04:00 -0700

    initial implementation of erb files

--- my-site.rb
+++ my-site.rb
@@ -4,20 +4,44 @@ require "sinatra/reloader"
 class MySite < Sinatra::Base
  	register Sinatra::Reloader
 
+	# get '/' do
+	# 	send_file './home.html'
+	# end
+
+	# get '/about' do
+	# 	send_file './about.html'
+	# end
+
+	# get '/blog' do
+	# 	send_file './blog.html'
+	# end
+
+	# get '/projects' do
+	# 	send_file './projects.html'
+	# end
+
 	get '/' do
-		send_file './home.html'
+		@title			= "Alice Rhomieux"
+		@description 	= "Hodor hodor hodor - hodor?"
+		erb :index
 	end
 
 	get '/about' do
-		send_file './about.html'
+		@title			= "Alice Rhomieux -- About"
+		@description 	= "About Alice Rhomieux -- a web developer in training in Seattle WA."
+		erb :about
 	end
 
 	get '/blog' do
-		send_file './blog.html'
+		@title			= "Bloggity blog blog"
+		@description 	= "A blog about the training experience of a new web developer."
+		erb :blog
 	end
 
 	get '/projects' do
-		send_file './projects.html'
+		@title			= "Alice Rhomieux -- Projects"
+		@description 	= "Classroom projects from Ada Developers Academy."
+		erb :projects
 	end
 
 end
