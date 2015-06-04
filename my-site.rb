d2f4728ac0ddb0d1c51f87475ad3e67f23127594
Kari <kari.lonczak@gmail.com>
2015-06-04 15:17:01 -0700

    Translated all static html file content into erb files. Removed static html files.

--- my-site.rb
+++ my-site.rb
@@ -5,27 +5,33 @@ class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
-  	send_file './public/index.html'
+    @header = "THOUGHTS"
+    erb :index
+  	# send_file './public/index.html'
   end
 
-  # get "/about_me" do
-  # 	send_file './static_html/about_me.html'
-  # end
-
-  # get "/pictures" do
-  # 	send_file './static_html/pictures.html'
-  # end
-
-  # get "/places" do
-  # 	send_file './static_html/places.html'
-  # end
-
-  # get "/projects" do
-  # 	send_file './static_html/projects.html'
-  # end
-
   get "/about_me" do
+    @header = "ABOUT ME"
     erb :about_me
+#   send_file './static_html/about_me.html'
+  end
+
+  get "/pictures" do
+    @header = "PICTURES"
+    erb :pictures
+    # send_file './static_html/pictures.html'
+  end
+
+  get "/places" do
+    @header = "PLACES"
+    erb :places
+    # send_file './static_html/places.html'
+  end
+
+  get "/projects" do
+    @header = "PROJECTS"
+    erb :projects
+    # send_file './static_html/projects.html'
   end
 
 end
