4a8b14e0c23366cfc0c63e5395da4c0d89ec0226
Kari <kari.lonczak@gmail.com>
2015-06-04 16:37:40 -0700

    WIP... Haven't committed in a while.

--- my-site.rb
+++ my-site.rb
@@ -7,31 +7,26 @@ class MySite < Sinatra::Base
   get "/" do
     @header = "THOUGHTS"
     erb :index
-  	# send_file './public/index.html'
   end
 
   get "/about_me" do
     @header = "ABOUT ME"
     erb :about_me
-#   send_file './static_html/about_me.html'
   end
 
   get "/pictures" do
     @header = "PICTURES"
     erb :pictures
-    # send_file './static_html/pictures.html'
   end
 
   get "/places" do
     @header = "PLACES"
     erb :places
-    # send_file './static_html/places.html'
   end
 
   get "/projects" do
     @header = "PROJECTS"
     erb :projects
-    # send_file './static_html/projects.html'
   end
 
 end
