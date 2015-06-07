5ea7039984e5f78d697e2405c36b27b1894ecd8d
Corinne Pingul <corinnepingul@gmail.com>
2015-06-07 12:09:40 -0700

    Added a contact.erb page and routed it via sinatra. Included a copyleft statement in footer using the current year.

--- my-site.rb
+++ my-site.rb
@@ -7,27 +7,33 @@ class MySite < Sinatra::Base
   register Sinatra::Reloader # makes line 2 active
 
   get "/" do
-    @pages = %w(home about projects blog)
+    @pages = %w(home about projects blog contact)
     @current = "home"
     erb :index
   end
 
   get "/about" do
-    @pages = %w(home about projects blog)
+    @pages = %w(home about projects blog contact)
     @current = "about"
     erb :about
   end
 
   get "/projects" do
-    @pages = %w(home about projects blog)
+    @pages = %w(home about projects blog contact)
     @current = "projects"
     erb :projects
   end
 
   get "/blog" do
-    @pages = %w(home about projects blog)
+    @pages = %w(home about projects blog contact)
     @current = "blog"
     erb :blog
+  end
+
+  get "/contact" do
+    @pages = %w(home about projects blog contact)
+    @current = "contact"
+    erb :contact
   end
 
 end
