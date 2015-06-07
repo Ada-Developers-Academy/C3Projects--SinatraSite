c3dde09f0692f6bd822545921d634af5253858c2
amirahaile <amira.dhaile@gmail.com>
2015-06-07 08:19:31 -0700

    Added <header> and <footer> as partials.

--- my-site.rb
+++ my-site.rb
@@ -3,12 +3,32 @@ require "sinatra/reloader"
 
 class MySite < Sinatra::Base
   register Sinatra::Reloader
+  PROJECTS = [
+    ["sinatra-site", "Sinatra Site", "2015-06-08", "June 2&ndash;8"],
+    ["bank-account", "Bank Account", "2015-05-31", "May 28&ndash;31"],
+    ["scrabble", "Scrabble", "2015-05-27", "May 26&ndash;27"],
+    ["farmar", "FarMar", "2015-05-22", "May 19&ndash;22"],
+    ["hangman", "Hangman", "2015-05-15", "May 14&ndash;15"],
+    ["solar-system", "Solar System", "2015-05-12", "May 12"],
+    ["orange-tree", "Orange Tree", "2015-05-11", "May 11"],
+    ["random-menu-generator", "Random Menu Generator", "2015-05-08", "May 8"],
+    ["calculator", "Calculator", "2015-05-07", "May 7"]
+  ]
 
   get "/" do
+    @title = "Ada Projects"
+    @projects = PROJECTS
     erb :index
   end
 
-  # get "projects" do
-  #   send_file 'public/projects.html'
-  # end
+  get "/:project" do
+    # @title, @project = params[:project]
+    project_url = params[:project]
+    project_info = PROJECTS.find { |url, project, datestamp, date| url == project_url}
+    @title = project_info[1]
+    @project = project_info[1]
+    @datestamp = project_info[2]
+    @date = project_info[3]
+    erb :project_page
+  end
 end
---
+++ todo.md
@@ -0,0 +1,25 @@
+# Sinatra Site To-Do List
+
+### Improvements
+
++ separate logic on `my-site.rb` to other module files
++ insert data for paragraphs on project page
++ separate the header and footer into partials
+
+### Bugs
+
++ `<footer>` disappears when browser height is < 744px
+
+### Styling Enhancements/Fixes
+
++ project tile borders could expand out on hover  
+
+      .projects:hover > .project {
+        padding: 3px 0;
+      }
+
++ contact header drop-down
+
++ get "get at me, yo." to change the line's hover color too
+
++ enable left & right scrolling
