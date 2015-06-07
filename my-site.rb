62a11bb310cbb2086c07d2a3f4bc0c00534dde1a
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-06-07 14:00:57 -0700

    DRYed up mysite.rb @title code, cleaned up formatting on styles.css

--- my-site.rb
+++ my-site.rb
@@ -5,35 +5,37 @@ require "./lib/pictures"
 class MySite < Sinatra::Base
  	register Sinatra::Reloader
 
+ 	BASE_TITLE 			= "A Rhx"
+ 	BASE_WITH_SEPARATOR = BASE_TITLE + " | "
+
 	get '/' do
-		@title			= "Alice Rhomieux"
+		@title			= BASE_TITLE
 		@description 	= "Hodor hodor hodor - hodor?"
 		erb :index
 	end
 
 	get '/about' do
-		@title			= "Alice Rhomieux | About"
-		@description 	= "About Alice Rhomieux -- a web developer in training in Seattle WA."
+		@title			= BASE_WITH_SEPARATOR + "about"
+		@description 	= "About A Rhx -- web developer in training in Seattle WA."
 		erb :about
 	end
 
 	get '/blog' do
-		@title			= "Bloggity blog blog"
+		@title			= BASE_WITH_SEPARATOR + "bloggity blog blog"
 		@description 	= "A blog about the training experience of a new web developer."
 		erb :blog
 	end
 
 	get '/projects' do
-		@title			= "Alice Rhomieux | Projects"
+		@title			= BASE_WITH_SEPARATOR + "projects"
 		@description 	= "Classroom projects from Ada Developers Academy."
 		erb :projects
 	end
 
 	get '/pictures' do
 		@pictures 		= Pictures::Picture.all_the_pics
-		@title			= "Pretty Pictures"
+		@title			= BASE_WITH_SEPARATOR + "pretty pictures"
 		@description 	= "Some pictures to look at. Mostly landscapes with the occasional fungus."
 		erb :pictures
 	end
-
 end
