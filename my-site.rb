0fad908bbe1315ed62b57814a7bc15f0ac5bf7bd
marleigh <camarleigh@gmail.com>
2015-06-07 20:05:08 -0700

    "checked all image tags to ensure closure cuz sinatra not working. Changed path route to statis instead of public."

--- my-site.rb
+++ my-site.rb
@@ -5,23 +5,23 @@ class MySite < Sinatra::Base
   register Sinatra::Reloader
 
   get "/" do
-    send_file'public/Home.html'
+    send_file'static/Home.html'
   end
 
   get "/about_me" do
-    send_file'public/Me.html'
+    send_file'static/Me.html'
   end
 
   get "/current_projects" do
-    send_file'public/Projects.html'
+    send_file'static/Projects.html'
   end
 
   get "/blog" do
-    send_file'public/Blog.html'
+    send_file'static/Blog.html'
   end
 
   get "/espresso" do
-    send_file 'public/Espresso.html'
+    send_file 'static/Espresso.html'
   end
 
 end
