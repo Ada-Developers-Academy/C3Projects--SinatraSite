dff7e30385609eb4be3a6a07e67534d320d47ada
Jeri <drvonnjerryxlii@gmail.com>
2015-06-09 22:40:16 -0700

    added 404 page & contact form that doesn't save content >_>

--- my-site.rb
+++ my-site.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 require "csv" # this is necessary for the projects stuff. n_n
 require_relative "lib/projects"
+# require_relative "lib/cthulhu"
 
 class MySite < Sinatra::Base
   register Sinatra::Reloader
@@ -28,11 +29,35 @@ class MySite < Sinatra::Base
     erb :projects
   end
 
-  # this is my absolute favorite part of this project.
+  get "/form" do
+    erb :form
+  end
+
+  post "/form" do
+    @form_data = params["form_data"]
+    @name = @form_data["name"]
+    @reply = @form_data["reply"]
+    @subject = @form_data["subject"]
+    @tags = @form_data["tags"]
+    @message = @form_data["message"]
+    # ThingsThatAreFun.new(params[:fun])
+    # Address.new(params[:addy])
+
+    erb :form
+  end
+
   get "/:project_key" do
     projects = Project.all_projects
     @project = projects["/" + params[:project_key]]
-    @name = @project.title
-    erb :project_form
+
+    unless @project.nil?
+      @name = @project.title
+      erb :project_form
+    else
+      # thanks to Loraine for the idea of using a 404 page! :D
+      # lesson learned: 404 has to be last or other pages will just 404 themselves.
+      @name = "404: Page Not Found"
+      erb :bad_address
+    end
   end
 end
