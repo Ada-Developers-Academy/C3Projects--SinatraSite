5d7ff05000d1d331f42208947fda9eb284f83151
Kari Bancroft <kariabancroft@gmail.com>
2015-10-26 09:51:48 -0700

    Revert "Wave 3- done!"

--- my-site.rb
+++ my-site.rb
@@ -1,39 +1,10 @@
 require "sinatra"
-require "sinatra/reloader" # alter code in sinatra so no restart needed
+require "sinatra/reloader"
 
 class MySite < Sinatra::Base
-  register Sinatra::Reloader # making line 2 active
+  register Sinatra::Reloader
 
   get "/" do
-    erb :home
-  end
 
-  get "/home" do
-    erb :home
   end
-
-  get "/about" do
-    erb :about
-  end
-
-  get "/projects" do
-    erb :projects
-  end
-
-  get "/blog" do
-    erb :blog
-  end
-
-  get "/nineties" do
-    erb :nineties
-  end
-
 end
-
-
-#   get "/blog" do
-#     send_file 'static/blog.html'
-#   end
-# end
-
-# rackup to execute
